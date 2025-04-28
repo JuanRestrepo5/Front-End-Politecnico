279f3f194ee3a03749b4c24a69d47a1ff4023385
JuanRestrepo5 <jmotato@poligran.edu.co>
2025-04-28 17:10:02 -0500

    Update server.js

--- pika-trading-backend/server.js
+++ pika-trading-backend/server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(Servidor corriendo en el puerto ${PORT});
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
 // Cargar usuarios desde JSON
